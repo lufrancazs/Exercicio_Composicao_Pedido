13cad9776143832323283903a88d6d54b8fac46b
Lucas Martins <lucasfranca_zs@hotmail.com>
2023-08-23 19:54:56 -0300

    Criado entidades, associação entre objetos e inicio do escopo do projeto

--- Application/Main.java
+++ Application/Main.java
@@ -1,9 +1,35 @@
 package Application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.println("Enter client data:");
+		
+		System.out.print("Name: ");
+		String clientName = sc.nextLine();
+		
+		System.out.print("Email: ");
+		String clientEmail = sc.nextLine();
+		
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		Date birth = sdf.parse(sc.next());
+		
+		System.out.println();
+		System.out.println("Enter order data:");
+		
+		
+		sc.close();
 	}
 
 }
---
+++ Entities/Order.java
@@ -0,0 +1,73 @@
+package Entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import Entities.Enum.OrderStatus;
+
+public class Order {
+	
+	private Date moment;
+	private OrderStatus status;
+	private Client client;
+	
+	List<OrderItem> orderItem = new ArrayList<>();
+	
+	public Order() {
+	}
+
+	public Order(Date moment, OrderStatus status, Client client) {
+		this.moment = moment;
+		this.status = status;
+		this.client = client;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public List<OrderItem> getOrderItem() {
+		return orderItem;
+	}
+	
+	public void addItem(OrderItem item) {
+		orderItem.add(item);
+	}
+	
+	public void removeItem(OrderItem item) {
+		orderItem.remove(item);
+	}
+	
+	public double total() {
+		
+		double sum = 0;
+		
+		for(OrderItem o : orderItem) {
+			
+			sum += o.subTotal();
+		}
+		return sum;
+	}
+
+}
